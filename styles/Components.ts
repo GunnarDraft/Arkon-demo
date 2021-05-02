673da9f464fee9b6d3cc3a74bd5e612d402392e2
gunnardraft <gunnardraft@live.com.mx>
2021-05-02 02:19:37 -0500

    update item tasks

--- styles/Components.ts
+++ styles/Components.ts
@@ -2,18 +2,16 @@ import styled from 'styled-components'
 
 export const TextFlex = styled.div`
   display: flex;
-  flex: 1;
-  /* color:#f1f1f1; */
-  
+  flex: 1; 
 `;
-
 export const Base = styled.div`
   display: flex;
   flex: 1;
-  flex-flow:column;
-  justify-content:flex-start;
+  flex-flow:row;
+  justify-content:space-evenly;
   align-items:stretch;
-  /* background-color:#111; */
+  border:1px solid red;
+  background-color:#e3e3e3;
   height:100vh;
 `;
 export const BoxFlex = styled.div`
@@ -27,17 +25,44 @@ export const BoxFlex = styled.div`
   border-radius:12px;
 `;
 export const TaskerContent = styled.div`
-  display: flex; 
-  align-items:flex-start;
-  flex-flow:column;
+  display: flex;  
+  flex-flow:row wrap;
+  justify-content:stretch;
   background-color:#eee; 
+  align-items:flex-start;
+  align-self:flex-start;
   padding:8px;
   margin:8px;
   z-index:2;
+  flex:1;
+  min-width:300px;
+  max-width:600px;
+  max-height:max-content;
+  min-height:min-content;
   box-sizing:border-box;
   border-radius:12px;
-  border:1px solid #222;
+  border:1px solid #443;
+  max-width:600px;
+  box-sizing:border-box;
 `;
+export const FlexLi = styled.div`
+  display:flex;
+  flex-flow:row nowrap;
+  justify-content:stretch;
+  `
+export const FlexUl = styled.div`
+  display:flex;
+  box-sizing:border-box;
+  flex-flow:column;
+  flex:1;
+  min-width:300px;
+  max-width:600px;
+  align-items:flex-start;
+  border:1px solid #2f2f2f;
+  padding:8px;
+  margin:8px;
+  border-radius:12px;
 
+`
 
 
