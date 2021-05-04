73c890e1c6e128fd5de30ae876584dd741fb978e
gunnardraft <gunnardraft@live.com.mx>
2021-05-04 06:56:28 -0500

    type fix

--- components/Tasker/History.tsx
+++ components/Tasker/History.tsx
@@ -2,7 +2,7 @@ import { GridContent, FlexData } from "../../styles/Components";
 import { GridColDef } from "@material-ui/data-grid";
 import { FC } from "react";
 
-export const History: FC<ITasks> = ({ tasks }: IHistory) => {
+export const History: FC<IHistory> = ({ tasks }: IHistory) => {
   const columns: GridColDef[] = [
     {
       field: "task",
